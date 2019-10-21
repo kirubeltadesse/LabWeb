8c2eef39614d2b05742e396437f3661cc67e369f
Kirubel M Tadesse <ktadesse@purdue.edu>
2019-10-21 12:45:52 -0400

    make slideshow for awards

--- Radiosim/urls.py
+++ Radiosim/urls.py
@@ -5,4 +5,17 @@ from Radiosim import views
 urlpatterns=[
     path('', views.IndexView.as_view(), name='index'),
     path('home', views.HomeView.as_view(), name='Home'),
+    path('research', views.ResearchView.as_view(), name='Research'),
+    path('media', views.MediaView.as_view(), name='Media'),
+    path('news', views.NewsView.as_view(), name='News'),
+    path('awards', views.AwardView.as_view(), name='Award'),
+    path('team', views.TeamView.as_view(), name='Team'),
+    path('publications', views.PublicationsView.as_view(), name='Publications'),
+    path('teaching', views.TeachView.as_view(), name='Teach'),
+    path('service', views.ServiceView.as_view(), name='Service'),
+    path('contact', views.ContactView.as_view(), name='Contact'),
+    path('chip', views.ChipView.as_view(), name='Chip'),
+    path('open', views.OpenView.as_view(), name='Open'),
+    path('hbc', views.HbcView.as_view(), name='Hbc'),
+    path('download', views.DownloadView.as_view(), name='Download'),
 ]
--- Radiosim/views.py
+++ Radiosim/views.py
@@ -6,10 +6,46 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
 class IndexView(TemplateView):
     template_name = 'Radiosim/index.html'
 
 class HomeView(TemplateView):
     template_name = 'Radiosim/index.html'
 
+class ResearchView(TemplateView):
+    template_name = 'Radiosim/research.html'
+
+class MediaView(TemplateView):
+    template_name = 'Radiosim/media.html'
+
+class NewsView(TemplateView):
+    template_name = 'Radiosim/news.html'
+class AwardView(TemplateView):
+    template_name = 'Radiosim/awards.html'
+
+class TeamView(TemplateView):
+    template_name = 'Radiosim/team.html'
+
+class PublicationsView(TemplateView):
+    template_name = 'Radiosim/publications.html'
+
+class TeachView(TemplateView):
+    template_name = 'Radiosim/teaching.html'
+
+class ServiceView(TemplateView):
+    template_name = 'Radiosim/service.html'
+
+class ContactView(TemplateView):
+    template_name = 'Radiosim/contact.html'
+
+class ChipView(TemplateView):
+    template_name = 'Radiosim/chip.html'
+
+class OpenView(TemplateView):
+    template_name = 'Radiosim/openings.html'
+
+class HbcView(TemplateView):
+    template_name = 'Radiosim/hbc.html'
+
+class DownloadView(TemplateView):
+    template_name = 'Radiosim/download.html'
